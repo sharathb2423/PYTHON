c958d953db0cedbf6d47e5eb4bbabcc2d15fe782
sharathb2423 <91074829+sharathb2423@users.noreply.github.com>
2021-10-22 20:49:43 +0530

    Add files via upload

---
+++ Sudoku Solver/sudoku solver.py
@@ -0,0 +1,69 @@
+def find_next_empty(puzzle):
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == 0:
+                return r, c
+    return None, None
+
+
+def is_valid(puzzle, guess, row, col):
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+
+    col_vals = []
+    for i in range(9):
+        col_vals.append(puzzle[i][col])
+    if guess in col_vals:
+        return False
+
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
+
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if puzzle[r][c] == guess:
+                return False
+    return True
+
+
+def solve_sudoku(puzzle):
+    row, col = find_next_empty(puzzle)
+
+    if row is None:
+        return True
+
+    for guess in range(1, 10):
+        if is_valid(puzzle, guess, row, col):
+            puzzle[row][col] = guess
+
+            if solve_sudoku(puzzle):
+                return True
+        puzzle[row][col] = -1
+
+    return False
+
+
+if __name__ == '__main__':
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+    for i in range(len(board[0])):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+        
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8: print(board[i][j])
+            else: print(str(board[i][j]) + " ", end="")
